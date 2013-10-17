07c67a1289addee6f9d82c5422322e7085e39fe5
moutou12 <shiaolin47@hotmail.com>
2013-10-17 09:54:33 +0000

    change web.js to read from file

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var app = express();
+
 app.use(express.logger());
 
+var buffer = new Buffer(fs.readFileSync('indoex.html','utf-8'), 'utf-8');
+var textout = buffer.toString('utf-8');
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(textout);
 });
 
 var port = process.env.PORT || 5000;
