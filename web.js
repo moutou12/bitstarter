7a806885c95dfdeacc8c5c719f89fb968f97c8eb
moutou12 <shiaolin47@hotmail.com>
2013-10-17 12:57:11 +0000

    read from html file

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
+
 app.use(express.logger());
 
+var buffer = new Buffer(fs.readFileSync('index.html','utf-8'), 'utf-8');
+var textout = buffer.toString('utf-8');
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(textout);
 });
 
 var port = process.env.PORT || 8080;
